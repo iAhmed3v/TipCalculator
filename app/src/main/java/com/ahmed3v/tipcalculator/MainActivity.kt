330972a59a8ab84729e1b879fcdeab3932c25382
iAhmed3v <devahmdali@gmail.com>
2022-07-20 14:37:00 +0300

    adding a function in MainActivity.kt to hide the keyboard

--- app/src/main/java/com/ahmed3v/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/ahmed3v/tipcalculator/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.ahmed3v.tipcalculator
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.ahmed3v.tipcalculator.databinding.ActivityMainBinding
 import java.text.NumberFormat
 import kotlin.math.ceil
@@ -16,7 +20,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.calculateBtn.setOnClickListener{ calculateTip() }
-    }
+
+        binding.costOfService.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
+
+        }
 
     private fun calculateTip(){
 
@@ -45,5 +52,16 @@ class MainActivity : AppCompatActivity() {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
 
         binding.tipResult.text = getString(R.string.tip_amount_text, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
